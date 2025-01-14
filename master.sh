70f8b7d8aa9745553cf8f7a7cd000405b22dc157
maath.j <mateua12392@gmail.com>
2025-01-14 16:44:40 -0300

    first commit

--- master.sh
+++ master.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo docker swarm init --advertise-addr=10.10.10.100
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+docker service create --name redis --replicas=5 redis:7.4.1
